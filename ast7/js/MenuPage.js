f88a4231d2d09670dfbc60f40f5aa7ff17d6a916
Shreejit <shreejit955@gmail.com>
2018-10-03 10:55:00 +0545

    managing code of ast7

--- ast7/js/MenuPage.js
+++ ast7/js/MenuPage.js
@@ -1,31 +1,31 @@
 let r=0;
 
-setTimeout(()=>{r=1;   console.log(r)},3000);
+setTimeout(()=>{r=1;   console.log(r)}, 3000);
 
 
 function Menu()
 {
-    con=document.getElementsByClassName("canvas")[0];
-    ctx=con.getContext("2d");
+    con = document.getElementsByClassName('canvas')[0];
+    ctx = con.getContext('2d');
 
 
-    let image=new Image();
-    image.src="images/fontPage.jpg";
+    let image = new Image();
+    image.src = 'images/fontPage.jpg';
     
-    ctx.drawImage(image,0,0);
+    ctx.drawImage( image, 0, 0);
 
-    ctx.fillStyle = "red";
-    ctx.font = "30px Arial";
-    ctx.fillText("Wait 3 Seconds Loading...",10,con.height-25);
+    ctx.fillStyle = 'red';
+    ctx.font = '30px Arial';
+    ctx.fillText( 'Wait 3 Seconds Loading...', 10, con.height - 25);
    
-    if(r===0)
+    if (r === 0)
     {
         requestAnimationFrame(Menu);
     }
-    else if(r===1){
+    else if (r === 1) {
      
         this.ctx.clearRect(0, 0, this.con.width, this.con.height);
-        var game=new Game(con);
+        var game = new Game(con);
         game.init();
        
     }
--- ast7/js/gameLogic.js
+++ ast7/js/gameLogic.js
@@ -1,146 +1,156 @@
 
-// startGame=()=>{
-//     con=document.getElementsByClassName("canvas")[0];
-//     var game=new Game(con);
-//     game.init();
-// }
+class Game {
+	constructor(canvas) {
 
-class Game
-{
-    constructor(canvas)
-    {
-        this.canvas=canvas;
-        this.context=this.canvas.getContext('2d');
+		this.canvas=canvas;
+		this.context=this.canvas.getContext('2d');
 
-        this.score=0;
-        this.dist=400;
-        this.pipes=[];
-        this.gravity=4;
-        this.menuPage=0;
-        this.running=1;
-        this.gameSpeed=2;
-        this.pipeNorthWidth=52;
-        this.pipeSouthWIdth=52;
-        this.pipeNorthHeight=242;
-        this.pipeSouthHeight=378;
+        
+        this.score = 0;
+        this.pipes = [];
+        this.rotate = 0;
+		this.GRAVITY = 4;
+		this.menuPage = 0;
+		this.running = 1;
+		this.gameSpeed = 2;
+        this.degreeRotate = 0.5
+        this.DISTTOBOTTOM = 400;
+        this.pipeNorthWidth = 52;
+		this.pipeSouthWIdth = 52;
+		this.pipeNorthHeight = 242;
+		this.pipeSouthHeight = 378;
         
         //creating objects
-        this.ground=new Ground("images/flappyfg.jpg",this.canvas.height);
-        this.player=new Player("images/Birdsprite.png");
-        //this.player=new Player("images/player2.png");
+        this.ground = new Ground('images/fg2.png',this.canvas.height);
+        this.player = new Player('images/Birdsprite.png');
+     
 
 
         //audio tack setup
-        this.die=new Audio();
-        this.die.src="audio/die.ogg";
-        this.wing=new Audio();
-        this.wing.src="audio/wing.ogg";
+        this.die = new Audio();
+        this.die.src = 'audio/die.ogg';
+        this.wing = new Audio();
+        this.wing.src = 'audio/wing.ogg';
         this.point=new Audio();
-        this.point.src="audio/point.ogg";
+        this.point.src ='audio/point.ogg';
 
         //loading pipes 
-        this.pipeNorth=new Image();
-        this.pipeNorth.src="images/pipeNorth.png";
-        this.pipeSouth=new Image();
-        this.pipeSouth.src="images/pipeSouth.png";
-        this.gameOver=new Image();
-        this.gameOver.src="images/gameover.png";
+        this.pipeNorth = new Image();
+        this.pipeNorth.src = 'images/pipeNorth.png';
+        this.pipeSouth= new Image();
+        this.pipeSouth.src = 'images/pipeSouth.png';
+        this.gameOver = new Image();
+        this.gameOver.src = 'images/gameover.png';
 
         //setting up lishner and obstacle
         this.pipes.push(new Pipe(600,0));
-        document.addEventListener("keydown",this.moveup.bind(this));
-        document.addEventListener("click",this.moveup.bind(this));
+        document.addEventListener('keydown',this.moveup.bind(this));
+        document.addEventListener('click',this.moveup.bind(this));
 
     }
 
-   moveup(event){
-      if(event.code==="Space")
-      {
+   moveup(event) {
+
+      if (event.code === 'Space') {
+
         this.player.moveUp();
         this.wing.play();
+
+      // this.degreeRotate=-0.5;
+      //  setTimeout(this.playerZero.bind(this),200)
       }
-      else if(event.code==="Enter" && this.running===0)
+
+      else if(event.code === 'Enter' && this.running === 0)
       {
-          this.score=0;
-          this.running=1;
+          this.score = 0;
+          this.running = 1;
           this.player.reset();
+          this.pipes = [];
+          this.pipes.push(new Pipe( 600, 0 ));
           
       }
     
    }
 
+   playerZero()
+   {
+    this.player.jumpzero();
+   }
+
   
 
     
-    decrementy(){
-        console.log("pring");
+    decrementy() {
+        console.log('pring');
     }
 
     
    
    init(){
        this.background=new Image();
-       this.background.src="images/flappyBackground.png";
+       this.background.src='images/flappyBackground.png';
        this.gameloop()
     };
 
 
     gameloop() {
-        if(this.running===1)
+        if (this.running===1)
         {
             this.update();
             this.render();
 
         }
-        else{
+        else {
             this.clearCanvas();
             this.drawBackground();
-            this.context.drawImage(this.gameOver,350,200,300,100);
+            this.context.drawImage( this.gameOver, 350, 200, 300, 100 );
         }   
         requestAnimationFrame(this.gameloop.bind(this));
     }
 
     update(){
-       this.player.y=this.player.y+this.gravity;
+       
+       this.player.y = this.player.y + this.GRAVITY + this.player.jumpVelocity;
+       if ( this.player.jumpVelocity < 0 )
+       {
+           this.player.jumpVelocity += 1;
+       }
        this.updatePipes();
         this.player.incrementIndex();
     }
 
     collisionCheck(pipe){
         
-            if(this.player.x+this.player.width>pipe.x
-                && this.player.x<=pipe.x+this.pipeNorthWidth
-                && ( this.player.y<=pipe.y+this.pipeNorthHeight || this.player.y+this.player.height>=pipe.y+this.dist)||
-                (this.player.y+this.player.height>=this.canvas.height-this.ground.height)
+            if ( this.player.x + this.player.width > pipe.x
+                && this.player.x <= pipe.x + this.pipeNorthWidth
+                && ( this.player.y <= pipe.y+this.pipeNorthHeight || this.player.y+this.player.height >= pipe.y+this.DISTTOBOTTOM) ||
+                ( this.player.y + this.player.height >= this.canvas.height - this.ground.height )
                 )
             {
-                this.die.play();
-                //location.reload();
-                this.running=0;
+               this.die.play();
+                this.running = 0;
             }
         
     }
 
     updatePipes()
     {
-        for(let pipe of this.pipes)
+        for ( let pipe of this.pipes )
         {
-            pipe.x=pipe.x-this.gameSpeed;
+            pipe.x = pipe.x - this.gameSpeed;
 
-            if(pipe.x===550)
+            if ( pipe.x === 550 )
             {
-                this.pipes.push(new Pipe(this.canvas.width,Math.floor(Math.random()*242)-242));
-               // this.pipes.push(new Pipe("images/pipeSouth.png"));
-                //console.log(this.pipes);
+                this.pipes.push(new Pipe(this.canvas.width, Math.floor(Math.random() * 242) - 242));
             }
 
-            if(pipe.x===-55)
+            if ( pipe.x === -55 )
             {
                 this.pipes.shift();
             }
             this.collisionCheck(pipe);
             
-            if(pipe.x===100)
+            if ( pipe.x === 100 )
             {
                 this.point.play();
                 this.increaseScore();
@@ -150,12 +160,11 @@ class Game
     }
 
 
-    increaseScore(){
+    increaseScore() {
         this.score++;
-
     }
 
-    render(){
+    render() {
        this.clearCanvas();
 
        //draw background
@@ -166,7 +175,7 @@ class Game
 
        this.drawObstacle();
 
-    //   this.drawForeGround();
+       this.drawForeGround();
 
        this.drawScore();
       
@@ -175,35 +184,45 @@ class Game
 
     drawScore()
     {
-        this.context.fillStyle="#000000";
-        this.context.font="20px Verdana";
-        this.context.fillText(`Score : ${this.score}`,20,30);
+        this.context.fillStyle = '#000000';
+        this.context.font = '20px Verdana';
+        this.context.fillText(`Score : ${this.score}`, 20, 30);
     }
 
-    drawForeGround(){
-        this.context.drawImage(this.ground.element,this.ground.x,this.ground.y);
+    drawForeGround() {
+        this.context.drawImage(this.ground.element, this.ground.x, this.ground.y);
     }
 
     drawObstacle()
     {
         for(let pipe of this.pipes)
         {
-            this.context.drawImage(this.pipeNorth,pipe.x,pipe.y);
-            this.context.drawImage(this.pipeSouth,pipe.x,pipe.y+this.dist);
+            this.context.drawImage(this.pipeNorth, pipe.x, pipe.y);
+            this.context.drawImage(this.pipeSouth, pipe.x,pipe.y + this.DISTTOBOTTOM);
         }
         
     }
 
     drawBackground()
     {
-        this.context.drawImage(this.background,0,0);
+        this.context.drawImage(this.background, 0, 0);
     }
 
     drawPlayer()
     {
        // console.log()
        // this.context.drawImage(this.player.element,0,0,768*this.player.index,550,this.player.x,this.player.y,80,80);
-       this.context.drawImage(this.player.element,768*this.player.index,0,768,550,this.player.x,this.player.y,this.player.width,this.player.height);
+       //this.context.save(); 
+       //console.log(this.rotate);
+        //this.context.translate(this.player.x,this.player.y);
+       //this.context.rotate(this.rotate * Math.PI / 180);
+       this.context.drawImage(this.player.element, 768 * this.player.index, 0, 768, 550, this.player.x, this.player.y, this.player.width, this.player.height);
+      // this.context.restore();
+
+       if( this.rotate < 20 ) {
+        this.rotate += this.degreeRotate;
+       }
+     
     }
 
     clearCanvas()
@@ -224,21 +243,27 @@ class Player
      */
     constructor(image)
     {
-        this.index=0;
-        this.y=0;
-        this.x=100;
-        this.width=50;
-        this.height=50;
-        this.jumpVelocity=60;
-        this.element=new Image();
-        this.element.src=image;  
+        this.index = 0;
+        this.y = 0;
+        this.x = 100;
+        this.width = 50;
+        this.height = 50;
+        this.jumpVelocity = 0;
+        this.element = new Image();
+        this.element.src = image;  
     }
 
-    moveUp(){ this.y-=this.jumpVelocity; }
+    moveUp(){ this.jumpVelocity = -12; }
 
-    incrementIndex(){ this.index= (this.index+1)%4 }
+    incrementIndex(){ this.index = (this.index + 1) % 4 }
 
-    reset(){this.y=0;this.x=100 }
+    reset() {
+
+        this.y = 0;  
+        this.x = 100 
+    }
+
+    jumpzero(){ this.jumpVelocity=0; }
 
 }
 
@@ -250,11 +275,11 @@ class Pipe{
      * @param {*} x the x coordinate of the pipe
      * @param {*} y the y coordinate of the pipe
      */
-    constructor(x,y)
+    constructor( x, y)
     {
-        this.x=x;
-        this.y=y;
-        this.height=242;
+        this.x = x;
+        this.y = y;
+        this.height = 242;
 
     } 
 }
@@ -267,16 +292,15 @@ class Ground{
      * @param {*} image :the image of the ground to be drawn
      * @param {*} canvasHeight :the hight of the canvas
      */
-    constructor(image,canvasHeight)
+    constructor( image, canvasHeight )
     {
-        this.element=new Image();
-        this.element.src=image;
-        this.height=218;
-        this.x=0;
-        this.y=canvasHeight-this.height;
+        this.element = new Image();
+        this.element.src = image;
+        this.height = 218;
+        this.x = 0;
+        this.y = canvasHeight-this.height;
         
     }
 }
 
 
-//startGame();
